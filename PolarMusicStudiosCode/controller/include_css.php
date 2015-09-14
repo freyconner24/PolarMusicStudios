31828a2bcd8480d0bd1546adc5fb618eadb56da5
Conner Frey <connerfrey@Conners-MacBook-Pro.local>
2015-09-13 21:24:40 -0700

    Updated the css to be https for google font api

--- PolarMusicStudiosCode/controller/include_css.php
+++ PolarMusicStudiosCode/controller/include_css.php
@@ -3,4 +3,4 @@
 <link rel="stylesheet" type="text/css" href="../static/css/global.css">
 <link rel="stylesheet" type="text/css" href="../static/css/header.css">
 <link rel="stylesheet" type="text/css" href="../static/css/contact.css">
-<link href='http://fonts.googleapis.com/css?family=Pacifico|Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Pacifico|Montserrat:400,700' rel='stylesheet' type='text/css'>
